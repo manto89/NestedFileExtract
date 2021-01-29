52c6cf70f31c60cf005cae61bd77415f79c341b3
Alessandro Mantoan <manto89@gmail.com>
2021-01-29 14:00:11 +0100

    fixed bug on parsing empty line

--- file_parser.py
+++ file_parser.py
@@ -43,7 +43,8 @@ def extract(zip_path, output_dir):
 def parse_file(path):
     first_file = True
     for line in open(path, 'r').readlines():
-        if len(line) < 1:
+        # skip empty lines (they can also have \n)
+        if len(line) < 2:
             continue
         if line[0] == '.':
             go_home()
